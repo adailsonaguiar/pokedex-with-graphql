a1981d18085956e08e0d24377241ff365639004a
unknown <adailsonacj@live.com>
2020-07-01 18:25:33 -0300

    Updates

---
+++ precache-manifest.20cd65880e3e2d3f660e9a40d3fb316f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee8b32b1c19d068f02a8c0326a7ae3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a171dd9e1a7322d091e",
+    "url": "/static/css/main.39329a5a.chunk.css"
+  },
+  {
+    "revision": "2b78c977ae9f03697930",
+    "url": "/static/js/2.10e994a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.10e994a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a171dd9e1a7322d091e",
+    "url": "/static/js/main.46143f90.chunk.js"
+  },
+  {
+    "revision": "a042f17e9d0b9986668c",
+    "url": "/static/js/runtime-main.fe27906a.js"
+  },
+  {
+    "revision": "bc3bf623e9722d13ff56801878fff5e0",
+    "url": "/static/media/load.bc3bf623.svg"
+  },
+  {
+    "revision": "2f0444725af93f3af837b2d3012fbe5c",
+    "url": "/static/media/pokebola.2f044472.svg"
+  }
+]);
